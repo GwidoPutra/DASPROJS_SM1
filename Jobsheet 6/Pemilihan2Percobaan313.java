733e4b54a135314ce1d65029b63461646e30a6a9
gwidopw <gwidoputra@gmail.com>
2023-10-09 10:55:53 +0700

    DoWhile13

--- Jobsheet 6/Pemilihan2Percobaan313.java
+++ Jobsheet 6/Pemilihan2Percobaan313.java
@@ -12,13 +12,22 @@ public class Pemilihan2Percobaan313 {
         System.out.println("Masukkan besarnya penghasilan :");
         penghasilan = input13.nextInt();
 
-        if (kategori.equalsIgnoreCase("Pekerja")){
+        if (kategori.equals("pekerja")){
             if (penghasilan <= 2000000)
                 pajak = 0.1;
             else if (penghasilan <= 3000000)
                 pajak = 0.15;
             else
                 pajak = 0.2;
+            gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.println("Penghasilan bersih : " + gajiBersih);
+        } else if (kategori.equals("pebisnis")){
+            if (penghasilan <= 2500000)
+                pajak = 0.15;
+            else if (penghasilan <= 3500000)
+                pajak = 0.2;
+            else
+                pajak = 0.25;
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.println("Penghasilan bersih : " + gajiBersih);
         } else 
