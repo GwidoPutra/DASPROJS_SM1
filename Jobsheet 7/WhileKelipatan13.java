9683ea9a1c9ae289ec6ef5938aa9d26d9c6be0b3
gwidopw <gwidoputra@gmail.com>
2023-10-13 23:49:48 +0700

    whilekelipatan

--- Jobsheet 7/WhileKelipatan13.java
+++ Jobsheet 7/WhileKelipatan13.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
-public class ForKelipatan13 {
+public class WhileKelipatan13 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int kelipatan, jumlah = 0, counter = 0;
+        int kelipatan, jumlah = 0, counter = 0, i = 1;
 
         System.out.print("Masukkan bilangan kelipatan (1-9) : ");
         kelipatan = scan.nextInt();
 
-        for (int i = 1; i <= 50; i++) {
+        while (i <= 50) {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter++;
             }
+            i++;
         }
 
         double average = (double) jumlah / counter;
