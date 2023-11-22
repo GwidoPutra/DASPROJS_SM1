1cb5b98db0d1afcc2139159ac09296faf3953af5
gwidopw <gwidoputra@gmail.com>
2023-11-23 05:24:37 +0700

    tugas

---
+++ Jobsheet 11/Tugas_1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Tugas_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N : ");
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = N; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}
