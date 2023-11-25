e2703afcf8e5861bde6eb403eae5e2ad485d534f
gwidopw <gwidoputra@gmail.com>
2023-11-25 18:21:02 +0700

    add

--- Jobsheet 13/UcapanTerimaKasih_13.java
+++ Jobsheet 13/UcapanTerimaKasih_13.java
@@ -13,4 +13,7 @@ public class UcapanTerimaKasih_13 {
         System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
 }
