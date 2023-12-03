69a1b87033f360509954867dfbe5e8306a02f14f
gwidopw <gwidoputra@gmail.com>
2023-12-04 00:14:18 +0700

    revisi

--- Jobsheet 13/UcapanTerimaKasih_13.java
+++ Jobsheet 13/UcapanTerimaKasih_13.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 
 public class UcapanTerimaKasih_13 {
-    public static String PenerimaUcapan(){
+    public static String penerimaUcapan(){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Tuliskan NAMA orang yang ingin Anda beri ucapan: ");
+        System.out.print("Tuliskan NAMA orang yang ingin anda beri ucapan: ");
         String namaOrang = sc.nextLine();
         sc.close();
         return namaOrang;
     }
-    public static void UcapanTerimaKasih(){
-        String nama = PenerimaUcapan();
-        System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+
-        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    public static void ucapanTerimakasih() {
+        String nama = penerimaUcapan();
+        System.out.println("Thank you " + nama + " for being the best teacher in the world \n" +
+        "you inspired in me a love for learning and made me feel like i could ask anything. ");
     }
-    public static void UcapanTambahan(String ucapanTambahan) {
+    public static void ucapanTambahan(String ucapanTambahan) {
         System.out.println(ucapanTambahan);
     }
     public static void main(String[] args) {
-        UcapanTerimaKasih();
-        ucapanTambahan("Thank you for the patience to guide us to become a better person");
+            ucapanTerimakasih();
+            ucapanTambahan("Thank you for the patience to guide us to become a better person");
     }
 }
-    
