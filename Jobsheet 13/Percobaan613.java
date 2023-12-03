cad4c4396052bba4cb063f41fe40d90a48d55ab3
gwidopw <gwidoputra@gmail.com>
2023-12-04 00:33:06 +0700

    tambahan

--- Jobsheet 13/Percobaan613.java
+++ Jobsheet 13/Percobaan613.java
@@ -1,21 +1,35 @@
-import java.util.Scanner;;
+import java.util.Scanner;
 public class Percobaan613 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
-        int p, l, t, L, vol;
 
-        System.out.print("Masukkan panjang : ");
+        int p,l,t,L,vol;
+
+        System.out.print("Masukkan panjang: ");
         p = input.nextInt();
-        System.out.print("Masukkan lebar : ");
+        System.out.print("Masukkan lebar: ");
         l = input.nextInt();
-        System.out.print("Masukkan tinggi : ");
+        System.out.print("Masukkan tinggi: ");
         t = input.nextInt();
 
-        L = p*l;
-        System.out.println("Luas persegi panjang adalah " +L);
+        L = hitungLuas(p, l) ;
+        System.out.println("Luas persegi panjang adalah " + L);
 
-        vol = p*l*t;
-        System.out.println("Volume balok adalah " +vol);
+        vol = hitungVolume(t, p, l);
+        System.out.println("Volume persegi panjang adalah " + vol);
+        
+        input.close();
     }
+
+    static int hitungLuas (int pjg,int lb){
+        int Luas = pjg*lb;
+        return Luas;
+    }
+
+    static int hitungVolume (int tinggi, int a, int b){
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
+    }
+    
+
 }
