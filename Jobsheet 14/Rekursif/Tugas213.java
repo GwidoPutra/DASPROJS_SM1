6067eb0a6590a04e791ea0fcde97f806edd3fd8f
gwidopw <gwidoputra@gmail.com>
2023-12-14 13:09:26 +0700

    commit

--- Jobsheet 14/Rekursif/Tugas213.java
+++ Jobsheet 14/Rekursif/Tugas213.java
@@ -1,70 +1,24 @@
 import java.util.Scanner;
 public class Tugas213 {
+
+    static int penjumlahanRekursif (int x, int bil) {
+        if (x == bil) {
+            System.out.print(bil + " = ");
+            return bil;
+        } else {
+            System.out.print(x + "+");
+            return (x + penjumlahanRekursif(x + 1, bil));
+        }
+    }
     
-    static void inputNilai(int [][] nilai, String [] nama) {
-        Scanner sc = new Scanner (System.in);
-        for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Nilai "+nama[i]);
-            for (int j = 0; j < nilai[i].length; j++) {
-                System.out.print("Nilai Minggu ke-"+(j+1)+" : ");
-                nilai[i][j] = sc.nextInt();
-            }
-            System.out.println();
-        }
-    }
-
-    static void tampilNilai(int [][] nilai, String [] nama) {
-        int i = 0;
-        for (int a[] : nilai) {
-            System.out.print("Nilai " + nama[i]+"\t: ");
-            for (int b : a) {
-                System.out.print(b + " ");
-            }
-            System.out.println();
-            i++;
-        }
-    }
-
-    static String cariMhsTertinggi (int [][] nilai, String [] nama) {
-        String NamaTertinggi = null;
-        int maks = nilai[0][0];
-        for (int i = 0; i < nilai.length; i++) {
-            for (int j = 0; j < nilai[i].length; j++) {
-                if (nilai[i][j] > maks) {
-                    maks = nilai [i][j];
-                    NamaTertinggi = nama[i];
-                }
-            }
-        }
-        return NamaTertinggi;
-    }
-
-    static void cariNilaiMaks (int [][] nilai, int []nilaiMaks) {
-        nilaiMaks[0] = nilai[0][0];
-        for (int i = 0; i < nilai.length; i++) {
-            for (int j = 0; j < nilai[i].length; j++) {
-                if (nilai[i][j] > nilaiMaks[0]) {
-                    nilaiMaks[0] = nilai [i][j];
-                    nilaiMaks[1] = j+1;
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        int [][] nilaiMhs = new int [5][7];
-        String []namaMhs = {"Sari", "Rina", "Yani", "Dwi", "Lusi"};
-        int []nilaiTertinggi = new int[2];
+        Scanner sc = new Scanner (System.in);
+        int angka;
 
-        inputNilai(nilaiMhs, namaMhs);
-
-        tampilNilai(nilaiMhs, namaMhs);
-
-        String mhsNilaiTertinggi = cariMhsTertinggi(nilaiMhs, namaMhs);
-        System.out.println("\nMahasiswa dengan nilai tertinggi adalah " + mhsNilaiTertinggi);
-
-        cariNilaiMaks(nilaiMhs, nilaiTertinggi);
-        System.out.println("Nilai tertinggi adalah " +nilaiTertinggi[0] + " di Minggu ke-" + nilaiTertinggi[1]);
-
+        System.out.print("Masukkan angka : ");
+        angka = sc.nextInt();
+        
+        System.out.println(penjumlahanRekursif(1, angka));
     }
+
 }
